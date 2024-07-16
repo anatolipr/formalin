69ba8a23ced203c27ed14fd80eb0e05078955a67
Anatoli Radulov <anatoli@impact.com>
2024-07-15 17:05:48 -0700

    wip

--- src/AppEdit.svelte
+++ src/AppEdit.svelte
@@ -28,6 +28,9 @@
         width: 100vw; 
         height: 100%; 
         position: fixed; 
+        z-index: 1000;
+        top:0;
+        left:0;
         display: flex; 
         align-items: center; 
         justify-content: center;
